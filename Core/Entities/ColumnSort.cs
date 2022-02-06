a8ec3034052967a9f49034ede156558d81d866a3
liuwei@tiny.station <liuweizzuie.com>
2022-02-06 20:42:29 +0800

    ColumnSort

---
+++ Core/Entities/ColumnSort.cs
@@ -0,0 +1,18 @@
+﻿using Galaxy.Libra.DapperExtensions.Predicate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowing.DDD.Core.Entities
+{
+    public class ColumnSort : ISort
+    {
+        public string PropertyName { get; set; }
+        public bool Ascending { get; set; }
+
+        public ColumnSort()
+        {
+
+        }
+    }
+}
---
+++ Core/Entities/IDSort.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowing.DDD.Core.Entities
+{
+    public class IDSort : ColumnSort
+    {
+        public IDSort()
+        {
+            this.PropertyName = "id";
+            this.Ascending = true;
+        }
+    }
+}
