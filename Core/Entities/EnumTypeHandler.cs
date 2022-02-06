4733e3f8dcb7df87ae18be7f371627f793d1c6e2
liuwei@tiny.station <liuweizzuie.com>
2022-02-06 20:41:57 +0800

    EnumTypeHandler

---
+++ Core/Entities/EnumTypeHandler.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
+using System.Text;
+
+namespace Snowing.DDD.Core.Entities
+{
+    public class EnumTypeHandler<T> : SqlMapper.TypeHandler<T> where T : struct, IConvertible
+    {
+        static EnumTypeHandler()
+        {
+            if (!typeof(T).IsEnum)
+            {
+                throw new ArgumentException("T must be an enumeration type");
+            }
+        }
+
+        public override T Parse(object value)
+        {
+            return (T)Enum.Parse(typeof(T), Convert.ToString(value));
+        }
+
+        public override void SetValue(IDbDataParameter parameter, T value)
+        {
+            parameter.Value = value.ToString();
+            parameter.DbType = DbType.AnsiString;
+        }
+    }
+}
